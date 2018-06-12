a0219c6b70d4d340178317a303aa37b5d86bd564
pawel <pbroniarczyk@outlook.com>
2018-06-12 22:54:13 +0200

    started drag&drop functionality - added events

--- src/components/board/card/Card.jsx
+++ src/components/board/card/Card.jsx
@@ -5,9 +5,26 @@ import "./card.css";
 
 
 class Card extends Component {
+    constructor(props) {
+        super(props);
+
+        this.dragCardStart = this.dragCardStart.bind(this);
+    }
+
+    dragCardStart = e => {
+        const sourceCard = e.currentTarget;
+
+        sourceCard.style.border = "dashed";
+        e.dataTransfer.setData("text/plain", e.target.id)
+    }
+
     render() {
         return (
-            <div>
+            <div
+                id="card_1"
+                className="card"
+                draggable="true"
+                onDragStart={this.dragCardStart}>
                 CARD
             </div>
         )
--- src/components/board/column/Column.jsx
+++ src/components/board/column/Column.jsx
@@ -8,9 +8,28 @@ import "./column.css";
 
 
 class Column extends Component {
+    constructor(props) {
+        super(props);
+
+        this.dropCardHandler = this.dropCardHandler.bind(this);
+    }
+
+    dropCardHandler = e => {
+        e.preventDefault();
+        const dropData = e.dataTransfer;
+
+        console.log("DROP TARGET", dropData);
+    }
+
+    componentDidMount = () => {
+        console.dir(document.querySelector(".column"))
+    }
+
     render() {
         return (
-            <div className="column">
+            <div
+                className="column"
+                onDrop={this.dropCardHandler}>
                 Column
                 <Card />
             </div>
